684913ea855fb8c3f9f761303126dfb51a3a36ca
Joel Weirauch <joel@weirau.ch>
2016-03-23 11:32:47 -0500

    fingers crossed...

--- src/FSClient.php
+++ src/FSClient.php
@@ -11,7 +11,7 @@ use \GuzzleHttp\Exception\ServerException;
  * API client for FeatureSwitches.com
  */
 class FSClient {
-    const VERSION = '0.8.2';
+    const VERSION = '0.8.3';
 
     protected $_customerKey;
     protected $_environmentKey;
